4cdb9ff986c0247e6d84c8d737ad05e0c0790218
Giggly Bits <71092239+greaselovely@users.noreply.github.com>
2020-12-28 17:02:59 -0700

    Create paloalto-dynupdates.sh
    Used for auditing (only) of dynamic updates across firewalls.  Dependency on using paloalto-configs.sh to download the device state, extract the running-config.xml and then grab all the dynamic update configs we are interested in.  This is an old script and does not have the mail function in it, but it's easy to add if you want.  Ye old copy and paste will do most of it if you need it.

---
+++ paloalto-dynupdates.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+################################
+########## SETTINGS ############
+
+subfolder="configs"
+source "${HOME}/panosh/paloalto-var.sh"
+
+if [ $1 ]
+	then equipment=$(grep -i $1 $inventory)
+	else equipment=$(cat $inventory)
+fi
+
+################################
+####### SETTINGS END############
+
+if [ ! "$(ls -A $dump)" ]
+	then 
+		echo "Getting configs..."
+		source "${HOME}/panosh/paloalto-configs.sh" $1
+fi
+
+if [ ! -d "$dyndump" ]
+	then 
+		mkdir -p "$dyndump"
+fi
+
+# starts the loop in the backup directory
+for f in "$dump/"*".tgz"
+	do 
+	echo "working on $f"
+	tar -xf "$f" -C "$dump" "./running-config.xml" 2>/dev/null
+	realname=$(xmllint --xpath "string(//hostname)" "$dump/running-config.xml" 2>/dev/null)
+	mv "$dump/running-config.xml" "$dump/$realname.xml" 2>/dev/null
+done
+
+if [ ! "$(ls -A $dump/*.xml 2>/dev/null)" ]
+	then 
+		echo "We have no files!!"
+		exit 0
+fi
+
+> "$dyndump/$win"
+
+for file in "$dump/"*".xml"; 
+	do 
+	realname=$(xmllint --xpath "string(//hostname)" "$file")
+	haenable=$(xmllint --xpath "string(//high-availability/enabled)" "$file")
+	tp=$(xmllint --xpath "string(//update-schedule//threats//action)" "$file")
+	av=$(xmllint --xpath "string(//update-schedule//anti-virus//action)" "$file")
+	wf=$(xmllint --xpath "string(//update-schedule//wildfire//action)" "$file")
+#	gp=$(xmllint --xpath "string(//update-schedule//global-protect-clientless-vpn//action)" "$file")
+	tpsync=$(xmllint --xpath "string(//update-schedule//threats//recurring//sync-to-peer)" "$file")
+	avsync=$(xmllint --xpath "string(//update-schedule//anti-virus//recurring//sync-to-peer)" "$file")
+	wfsync=$(xmllint --xpath "string(//update-schedule//wildfire//recurring//sync-to-peer)" "$file")
+	tptime=$(xmllint --xpath "string(//update-schedule//threats//recurring//at)" "$file")
+	avtime=$(xmllint --xpath "string(//update-schedule//anti-virus//recurring//at)" "$file")
+	wftime=$(xmllint --xpath "string(//update-schedule//wildfire//recurring//at)" "$file")
+#	gptime=$(xmllint --xpath "string(//update-schedule//global-protect-clientless-vpn//recurring//at)" "$file")
+	tpwhen=$(xmlstarlet sel -t -c "//threats/recurring/*" "$file" | head -1 | sed -e 's/<//g' -e 's/>//g' -e 's/sync-to-peer//g' -e 's/yes\///g' -e 's/no\///g')
+	avwhen=$(xmlstarlet sel -t -c "//anti-virus/recurring/*" "$file" | head -1 | sed -e 's/<//g' -e 's/>//g' -e 's/sync-to-peer//g' -e 's/yes\///g' -e 's/no\///g')
+	wfwhen=$(xmlstarlet sel -t -c "//wildfire/recurring/*" "$file" | head -1 | sed -e 's/<//g' -e 's/>//g' -e 's/sync-to-peer//g' -e 's/yes\///g' -e 's/no\///g')
+#	gpwhen=$(xmlstarlet sel -t -c "//global-protect-clientless-vpn/recurring/*" "$file" | head -1 | sed -e 's/<//g' -e 's/>//g' -e 's/sync-to-peer//g' -e 's/yes\///g' -e 's/no\///g')
+	
+	
+	if [ "$haenable" = "yes" ];
+		then echo -e "$realname (HA ENABLED)" >> "$dyndump/$win"
+		else echo -e "$realname" >> "$dyndump/$win"
+	fi
+
+	if [ -z "$tpsync" ];
+		then echo -e "\tTP = "$tp $tpwhen $tptime >> "$dyndump/$win"
+		else echo -e "\tTP = "$tp $tpwhen $tptime "sync-to-peer:" $tpsync >> "$dyndump/$win"
+	fi
+	if [ -z "$avsync" ];
+		then echo -e "\tAV = "$av $avwhen $avtime >> "$dyndump/$win"
+		else echo -e "\tAV = "$av $avwhen $avtime "sync-to-peer:" $avsync >> "$dyndump/$win"
+	fi
+	if [ -z "$wfsync" ];
+		then echo -e "\tWF = "$wf $wfwhen $wftime >> "$dyndump/$win"
+		else echo -e "\tWF = "$wf $wfwhen $wftime "sync-to-peer:" $wfsync >> "$dyndump/$win"
+	fi
+#	if [ -z "$gpsync" ];
+#		then echo -e "\tGP = "$gp $gpwhen $gptime >> "$dyndump/$win"
+#		else echo -e "\tGP = "$gp $gpwhen $gptime "sync-to-peer:" $gpsync >> "$dyndump/$win"
+#	fi
+	if [ "$tp" = "download-and-install" ] || [ -z "$tp" ];
+		then echo
+		else echo -e "\tFix $realname threats - it is set to $tp" | tr '[:lower:]' '[:upper:]' >> "$dyndump/$win"
+	fi
+	if [ "$av" = "download-and-install" ] || [ -z "$av" ];
+		then echo
+		else echo -e "\tFix $realname anti-virus - it is set to $av " | tr '[:lower:]' '[:upper:]' >> "$dyndump/$win"
+	fi
+	if [ "$wf" = "download-and-install" ] || [ -z "$wf" ];
+		then echo
+		else echo -e "\tFix $realname wildfire - it is set to $wf " | tr '[:lower:]' '[:upper:]' >> "$dyndump/$win"
+	fi
+	if [ "$gp" = "download-and-install" ] || [ -z "$gp" ];
+		then echo
+		else echo -e "Fix $realname wildfire - it is set to $gp " | tr '[:lower:]' '[:upper:]' >> "$dyndump/$win"
+	fi
+	
+echo -e " " >> "$dyndump/$win"
+	
+done;
+
+
+#clean up
+rm "$dump/"*".xml"
+cat "$dyndump/$win"
